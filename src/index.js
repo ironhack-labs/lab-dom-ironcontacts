bce0ae43776448e012a7a3efad66c8d54fea5ea5
Max Wellings <maxmechlin@gmail.com>
2024-01-24 19:57:54 +0100

    lab solved

--- src/index.js
+++ src/index.js
@@ -39,19 +39,52 @@ tableBody.appendChild(exampleRow);
 const threeContacts = contacts.splice(0, 3);
 
 // Your code goes here ...
+ threeContacts.forEach( contact => {
+  const row = document.createElement("tr")
+  row.innerHTML = `
+  <td>
+    <img src="${contact.pictureUrl}" />
+  </td>
+  <td> ${contact.name} </td>
+  <td> ${contact.popularity.toFixed(2)} </td>
+  <td>
+    <button class="btn-delete">Delete</button>
+  </td>
+  <td>
+    <button class="btn-like">
+      <img src="./images/icon.png" alt="like" />
+    </button>
+  </td>
+`;
+tableBody.appendChild(row);
+
+
 
 
   
   // ITERATION 2 - Delete Buttons
   
   // Your code goes here ...
-  
+  const btn = row.querySelector(".btn-delete");
+
+
+  btn.addEventListener("click", (event) => {
+    console.log(event.target)
+    row.remove()
+  })
+
   
 
   // ITERATION 3 - Like Buttons
 
   // Your code goes here ...
+  const likeBtn = row.querySelector(".btn-like");
 
+  likeBtn.addEventListener("click", (event) => {
+    console.log(event.target)
+      likeBtn.classList.toggle("selected")
+  })
+})
   
   
 
@@ -59,3 +92,27 @@ const threeContacts = contacts.splice(0, 3);
 // Bonus: ITERATION 4 - Add Random Contacts
 
 // Your code goes here ...
+  const randomBtn = document.querySelector("#btn-add-random")
+
+  randomBtn.addEventListener("click", (event) => {
+    const randomIndexTwo = Math.floor(Math.random() * contacts.length);
+    const splicedArrTwo = contacts.splice(randomIndexTwo, 1);
+    const randomContactTwo = splicedArrTwo[0];
+    const rowBonus = document.createElement("tr")
+    rowBonus.innerHTML = `
+    <td>
+      <img src="${randomContactTwo.pictureUrl}" />
+    </td>
+    <td> ${randomContactTwo.name} </td>
+    <td> ${randomContactTwo.popularity.toFixed(2)} </td>
+    <td>
+      <button class="btn-delete">Delete</button>
+    </td>
+    <td>
+      <button class="btn-like">
+        <img src="./images/icon.png" alt="like" />
+      </button>
+    </td>
+  `;
+  tableBody.appendChild(rowBonus);
+  })
