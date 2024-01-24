8e65b923ed9ef63a83426ffb2f5bfb587eec4de8
Stefan.Kummerlöw <gummiz@gmx.net>
2024-01-24 15:44:44 +0100

    done

--- src/index.js
+++ src/index.js
@@ -33,15 +33,7 @@ tableBody.appendChild(exampleRow);
 const copyContactsArr = [...contacts];
 const threeContacts = copyContactsArr.splice(0, 3);
 
-// Splice 3 element from the contacts array at the random index
-// const randomIndex2 = Math.floor(Math.random() * threeContacts.length);
-// const splicedArr2 = threeContacts.splice(randomIndex2, 3);
-
-// Get the element from the spliced array
-// const threeContacts = splicedArr2[0];
-
 // Your code goes here ...
-// console.log(threeContacts);
 
 //new row
 threeContacts.forEach((contact) => {
@@ -69,36 +61,80 @@ threeContacts.forEach((contact) => {
 
 // Your code goes here ...
 // find all delete btn
-const delBtnAll = document.querySelectorAll(".btn-delete")
+const delBtnAll = document.querySelectorAll(".btn-delete");
 
-delBtnAll.forEach(btn =>{
+delBtnAll.forEach((btn) => {
   btn.addEventListener("click", function (btnElm) {
-    console.log("moin")
+    console.log("moin");
     //find closest Element of tr and remove it
-    btnElm.target.closest("tr").remove()
-  })
-  
-})
+    btnElm.target.closest("tr").remove();
+  });
+});
 
 console.log();
-
 
 // ITERATION 3 - Like Buttons
 
 // Your code goes here ...
-// find all like buttons 
-const likeBtnAll = document.querySelectorAll(".btn-like")
+// find all like buttons
+const likeBtnAll = document.querySelectorAll(".btn-like");
 
-likeBtnAll.forEach(likebtn => {
+likeBtnAll.forEach((likebtn) => {
   // console.log(likebtn)
-  likebtn.addEventListener("click", btnElm =>{
-    likebtn.classList.toggle("selected")
-  })
-})
-
-
-
+  likebtn.addEventListener("click", (btnElm) => {
+    likebtn.classList.toggle("selected");
+  });
+});
 
 // Bonus: ITERATION 4 - Add Random Contacts
 
 // Your code goes here ...
+
+const addContactBtn = document.getElementById("btn-add-random");
+
+addContactBtn.addEventListener("click", () => {
+  console.log("ADDD");
+  const randomIndex3 = Math.floor(Math.random() * contacts.length);
+  const newRandSplice = contacts.splice(randomIndex3, 1);
+  // Get the element from the spliced array
+  const newContact = newRandSplice[0];
+  console.log(newContact);
+
+  const newRow = document.createElement("tr");
+  newRow.innerHTML = `
+  <td>
+    <img src="${newContact.pictureUrl}" />
+  </td>
+  <td> ${newContact.name} </td>
+  <td> ${newContact.popularity.toFixed(2)} </td>
+  <td>
+    <button class="btn-delete">Delete</button>
+  </td>
+  <td>
+    <button class="btn-like">
+      <img src="./images/icon.png" alt="like" />
+    </button>
+  </td>
+`;
+
+  tableBody.appendChild(newRow);
+
+  const delBtnAllNew = document.querySelectorAll(".btn-delete");
+
+  delBtnAllNew.forEach((btn) => {
+    btn.addEventListener("click", function (btnElm) {
+      console.log("moin");
+      //find closest Element of tr and remove it
+      btnElm.target.closest("tr").remove();
+    });
+  });
+
+  const likeBtnAllNew = document.querySelectorAll(".btn-like");
+
+  likeBtnAllNew.forEach((likebtn) => {
+    // console.log(likebtn)
+    likebtn.addEventListener("click", (btnElm) => {
+      likebtn.classList.toggle("selected");
+    });
+  });
+});
