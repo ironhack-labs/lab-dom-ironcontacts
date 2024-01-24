01388f69eee69923a71803d97f4d2313ece93111
erik busch <eazyErik>
2024-01-24 20:24:38 +0100

    rounded popularity to 2 decimals

--- src/index.js
+++ src/index.js
@@ -45,7 +45,7 @@ const threeContacts = contacts.splice(0, 3);
       <img src="${contact.pictureUrl}" />
     </td>
     <td> ${contact.name} </td>
-    <td> ${contact.popularity} </td>
+    <td> ${contact.popularity.toFixed(2)} </td>
     <td>
     <button class="btn-delete">Delete</button>
     </td>
