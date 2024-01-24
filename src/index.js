7de29656559ecaa2c9f256ebff66a41e2d2cb2cc
Maria Nogales <maria.nogales99@gmail.com>
2024-01-24 19:28:30 +0100

    super sad

--- src/index.js
+++ src/index.js
@@ -40,20 +40,57 @@ const threeContacts = contacts.splice(0, 3);
 
 // Your code goes here ...
 
+threeContacts.forEach((eachFamous) => {
+  const nextRow = document.createElement("tr");
+  nextRow.innerHTML = `
+  <td>
+    <img src="${eachFamous.pictureUrl}" />
+  </td>
+  <td> ${eachFamous.name} </td>
+  <td> ${eachFamous.popularity.toFixed(2)} </td>
+  <td>
+    <button class="btn-delete">Delete</button>
+  </td>
+  <td>
+    <button class="btn-like">
+      <img src="./images/icon.png" alt="like" />
+    </button>
+  </td>
+`
+  tableBody.appendChild(nextRow);
 
-  
   // ITERATION 2 - Delete Buttons
-  
+
   // Your code goes here ...
-  
-  
+  let deleteButton = nextRow.querySelector('.btn-delete')
+  deleteButton.onclick = () => {
+
+    nextRow.remove(eachFamous)
+  }
 
   // ITERATION 3 - Like Buttons
 
   // Your code goes here ...
+  let likeButton = nextRow.querySelector('.btn-like')
+  likeButton.onclick = () => {
+    likeButton.style.background_color = 'pink'
+  }
 
-  
-  
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // Bonus: ITERATION 4 - Add Random Contacts
