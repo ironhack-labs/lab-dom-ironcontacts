64ef3d4a28c5f03f13f5a996d8d3498e1f706afc
Leonardo Oliveira <leooliveira1025@gmail.com>
2024-01-25 08:31:44 +0000

    lab solved without bonus question

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 // HTML ELEMENTS
 const buttonAddRandom = document.querySelector("#btn-add-random");
 const tableBody = document.querySelector("tbody#contacts");
-
-
 // ITERATION 0 | Example Row
 // Splice 1 element from the contacts array at the random index
 const randomIndex = Math.floor(Math.random() * contacts.length);
 const splicedArr = contacts.splice(randomIndex, 1);
-
 // Get the element from the spliced array
 const randomContact = splicedArr[0];
-
 const exampleRow = document.createElement("tr");
 exampleRow.innerHTML = `
   <td>
@@ -27,35 +23,46 @@ exampleRow.innerHTML = `
     </button>
   </td>
 `;
-
 tableBody.appendChild(exampleRow);
-
-
-
-
-
 // ITERATION 1 - Display 3 contacts
 // Get the first 3 contacts from the 'contacts' array.
 const threeContacts = contacts.splice(0, 3);
-
 // Your code goes here ...
-
-
-  
+threeContacts.forEach((contact) => {
+  const newRow = document.createElement("tr");
+  newRow.innerHTML = `
+  <td>
+    <img src="${contact.pictureUrl}" />
+  </td>
+  <td> ${contact.name} </td>
+  <td> ${contact.popularity.toFixed(2)} </td>
+  <td>
+    <button class="btn-delete">Delete</button>
+  </td>
+  <td>
+    <button class="btn-like">
+      <img src="./images/icon.png" alt="like" />
+    </button>
+  </td>
+`;
+  tableBody.appendChild(newRow);
+});
   // ITERATION 2 - Delete Buttons
-  
   // Your code goes here ...
-  
-  
-
+  const deleteBtn = document.querySelectorAll(".btn-delete");
+  deleteBtn.forEach((deleteButton) => {
+    deleteButton.addEventListener("click", function () {
+      const rowToRemove = this.parentElement.parentElement;
+      rowToRemove.remove();
+    });
+  });
   // ITERATION 3 - Like Buttons
-
   // Your code goes here ...
-
-  
-  
-
-
+  const likeBtn = document.querySelectorAll(".btn-like");
+  likeBtn.forEach((likeButton) => {
+    likeButton.addEventListener("click", function () {
+      this.classList.toggle("selected");
+    });
+  });
 // Bonus: ITERATION 4 - Add Random Contacts
-
 // Your code goes here ...
