7fa9c16d95a5507d618246c1f30449b6309d2a0d
Büsra Kocer <busustaoglu@gmail.com>
2024-01-25 01:51:16 +0100

    done

--- src/index.js
+++ src/index.js
@@ -40,22 +40,46 @@ const threeContacts = contacts.splice(0, 3);
 
 // Your code goes here ...
 
-
-  
+threeContacts.forEach((contact) => {
+  const newRow = document.createElement("tr");
+  newRow.innerHTML = `
+    <td>
+      <img src="${contact.pictureUrl}" />
+    </td>
+    <td>${contact.name}</td>
+    <td>${contact.popularity.toFixed(2)}</td>
+    <td>
+      <button class="btn-delete">Delete</button>
+    </td>
+    <td>
+      <button class="btn-like">
+        <img src="./images/icon.png" alt="like" />
+      </button>
+    </td>
+  `;
+  tableBody.appendChild(newRow);
   // ITERATION 2 - Delete Buttons
   
   // Your code goes here ...
   
-  
+  const deleteButton = newRow.querySelector(".btn-delete");
+  deleteButton.addEventListener("click", () => {
+    newRow.remove();
+  });
 
   // ITERATION 3 - Like Buttons
 
   // Your code goes here ...
 
-  
+  const likeButton = newRow.querySelector(".btn-like");
+  likeButton.addEventListener("click", () => {
+    newRow.classList.toggle("selected");
+  });
+});
   
 
 
 // Bonus: ITERATION 4 - Add Random Contacts
 
 // Your code goes here ...
+
