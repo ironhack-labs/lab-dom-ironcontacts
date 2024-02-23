f189c5273afb58f6d19360cbfa9eb9800125adf2
Brenni1 <phillip.brenndoerfer@gmail.com>
2024-02-23 15:30:19 +0100

    Finished the Lab

--- src/index.js
+++ src/index.js
@@ -36,22 +36,61 @@ tableBody.appendChild(exampleRow);
 
 // ITERATION 1 - Display 3 contacts
 // Get the first 3 contacts from the 'contacts' array.
-const threeContacts = contacts.splice(0, 3);
+
+const threeContacts = contacts.splice(0 , 3);
+
 
 // Your code goes here ...
+
+threeContacts.forEach((contact) => {
+
+  const newContacts = document.createElement("tr") ;
+
+  
+    newContacts.innerHTML =  `
+    <td>
+      <img src="${contact.pictureUrl}" />
+    </td>
+    <td> ${contact.name} </td>
+    <td> ${contact.popularity.toFixed(1)} </td>
+    <td>
+      <button class="btn-delete">Delete</button>
+    </td>
+    <td>
+      <button class="btn-like">
+        <img src="./images/icon.png" alt="like" />
+      </button>
+    </td>
+    `;
+
+    
+
 
 
   
   // ITERATION 2 - Delete Buttons
   
   // Your code goes here ...
-  
-  
+  let deleteButton = newContacts.querySelector(".btn-delete") ;
+// use of .onclick, since it's shorter code and there is only one event to be handled
+  deleteButton.onclick = function() {
+    newContacts.remove()
+  }
+
 
   // ITERATION 3 - Like Buttons
 
   // Your code goes here ...
 
+  let likeButton = newContacts.querySelector(".btn-like") ;
+
+// use of .onclick, since it's shorter code and there is only one event to be handled
+  likeButton.onclick = function() {
+    likeButton.classList.toggle("selected")
+  }
+
+  tableBody.appendChild(newContacts);
+});
   
   
 
