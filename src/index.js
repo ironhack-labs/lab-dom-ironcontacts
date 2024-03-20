0223f37bf9352d3eb9bc94f133f5137a0e3307a5
johndaniell@yahoo.com <johndaniell@yahoo.com>
2024-03-20 14:19:13 +0100

    done

--- src/index.js
+++ src/index.js
@@ -11,13 +11,16 @@ const splicedArr = contacts.splice(randomIndex, 1);
 // Get the element from the spliced array
 const randomContact = splicedArr[0];
 
-const exampleRow = document.createElement("tr");
-exampleRow.innerHTML = `
+
+// A function to add elements
+function addRowWithData(contactsToAdd) {
+  const addedRow = document.createElement("tr");
+  addedRow.innerHTML = `
   <td>
-    <img src="${randomContact.pictureUrl}" />
+    <img src="${contactsToAdd.pictureUrl}" alt="photo" />
   </td>
-  <td> ${randomContact.name} </td>
-  <td> ${randomContact.popularity.toFixed(2)} </td>
+  <td> ${contactsToAdd.name} </td>
+  <td> ${contactsToAdd.popularity.toFixed(2)} </td>
   <td>
     <button class="btn-delete">Delete</button>
   </td>
@@ -28,10 +31,25 @@ exampleRow.innerHTML = `
   </td>
 `;
 
-tableBody.appendChild(exampleRow);
+  const deleteButton = addedRow.querySelector(".btn-delete");
+  const likeButton = addedRow.querySelector(".btn-like");
+
+  deleteButton.addEventListener("click", () => {
+    contacts.push(contactsToAdd);
+    addedRow.remove();
+    buttonAddRandom.disabled = false;
+  });
+
+  likeButton.addEventListener("click", () => {
+    likeButton.classList.toggle("selected")
+  });
+
+  tableBody.appendChild(addedRow);
+}
 
 
-
+// adding the first random element
+addRowWithData(randomContact)
 
 
 // ITERATION 1 - Display 3 contacts
@@ -41,21 +59,32 @@ const threeContacts = contacts.splice(0, 3);
 // Your code goes here ...
 
 
+//adding three contacts
+threeContacts.forEach(contact => addRowWithData(contact));
   
-  // ITERATION 2 - Delete Buttons
-  
-  // Your code goes here ...
-  
+
+
+// ITERATION 2 - Delete Buttons
+// added in the function
   
 
   // ITERATION 3 - Like Buttons
-
   // Your code goes here ...
 
   
   
 
+// Bonus: ITERATION 4 - Add Random Contact
+// grabbing the add Random COntact button
 
-// Bonus: ITERATION 4 - Add Random Contacts
+buttonAddRandom.addEventListener("click", () => {
+  if (contacts.length > 0) {
 
-// Your code goes here ...
+    const randomIndex = Math.floor(Math.random() * contacts.length);
+    const randomContact = contacts.splice(randomIndex, 1)[0];
+    addRowWithData(randomContact);
+  } else {
+    console.log("No more contacts to add.");
+    buttonAddRandom.disabled = true;
+  }
+});
