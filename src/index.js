9e5d696067c25db1d9b1abaaa35579d5023c03c7
Laura Pinto <laurampinto90@gmail.com>
2024-03-21 18:58:53 +0000

    all iteration

--- src/index.js
+++ src/index.js
@@ -39,12 +39,36 @@ tableBody.appendChild(exampleRow);
 const threeContacts = contacts.splice(0, 3);
 
 // Your code goes here ...
-
+threeContacts.forEach(contact => {
+  const newRow = document.createElement("tr");
+  newRow.innerHTML = `
+  <td>
+    <img src="${contact.pictureUrl}" />
+  </td>
+  <td> ${contact.name} </td>
+  <td> ${contact.popularity.toFixed(2)} </td>
+  <td>
+    <button class="btn-delete">Delete</button>
+  </td>
+  <td>
+    <button class="btn-like">
+      <img src="./images/icon.png" alt="like" />
+    </button>
+  </td>
+  `;
+ 
 
   
   // ITERATION 2 - Delete Buttons
   
   // Your code goes here ...
+
+  const deleteButton = newRow.querySelector(".btn-delete");
+
+  deleteButton.addEventListener("click", () => {
+    newRow.remove();
+  });
+
   
   
 
@@ -52,10 +76,57 @@ const threeContacts = contacts.splice(0, 3);
 
   // Your code goes here ...
 
-  
+  const likeButton = newRow.querySelector(".btn-like");
+
+  likeButton.addEventListener("click", () => {
+    newRow.classList.toggle("selected");
+  });
+
+  tableBody.appendChild(newRow);
+});
   
 
 
 // Bonus: ITERATION 4 - Add Random Contacts
 
 // Your code goes here ...
+
+
+buttonAddRandom.addEventListener("click", () => {
+  const randomIndex = Math.floor(Math.random() * contacts.length);
+  const randomContact = contacts[randomIndex];
+
+  const newRow = document.createElement("tr");
+  newRow.innerHTML = `
+  <td>
+    <img src="${randomContact.pictureUrl}" />
+  </td>
+  <td> ${randomContact.name} </td>
+  <td> ${randomContact.popularity.toFixed(2)} </td>
+  <td>
+    <button class="btn-delete">Delete</button>
+  </td>
+  <td>
+    <button class="btn-like">
+      <img src="./images/icon.png" alt="like" />
+    </button>
+  </td>
+  `;
+
+  const deleteButton = newRow.querySelector(".btn-delete");
+
+  deleteButton.addEventListener("click", () => {
+    newRow.remove();
+  });
+
+  const likeButton = newRow.querySelector(".btn-like");
+
+  likeButton.addEventListener("click", () => {
+    newRow.classList.toggle("selected");
+  });
+
+  tableBody.appendChild(newRow);
+
+});
+
+
