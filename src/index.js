da2d9938dc0271d07134a7d571aa1e2d002f1f61
VinothaArivalagan <vinothag96@gmail.com>
2024-03-23 13:12:12 +0100

    ironContacts

--- src/index.js
+++ src/index.js
@@ -11,13 +11,15 @@ const splicedArr = contacts.splice(randomIndex, 1);
 // Get the element from the spliced array
 const randomContact = splicedArr[0];
 
-const exampleRow = document.createElement("tr");
-exampleRow.innerHTML = `
+const renderRow = contact => {
+
+const newRow = document.createElement("tr");
+newRow.innerHTML = `
   <td>
-    <img src="${randomContact.pictureUrl}" />
+    <img src="${contact.pictureUrl}" />
   </td>
-  <td> ${randomContact.name} </td>
-  <td> ${randomContact.popularity.toFixed(2)} </td>
+  <td> ${contact.name} </td>
+  <td> ${contact.popularity.toFixed(2)} </td>
   <td>
     <button class="btn-delete">Delete</button>
   </td>
@@ -28,31 +30,48 @@ exampleRow.innerHTML = `
   </td>
 `;
 
-tableBody.appendChild(exampleRow);
+
+tableBody.appendChild(newRow);
 
 
 
+}
 
 
 // ITERATION 1 - Display 3 contacts
 // Get the first 3 contacts from the 'contacts' array.
-const threeContacts = contacts.splice(0, 3);
+const threeContacts = contacts.splice(0, 3)
+
+threeContacts.forEach(currentContact=> {
+  renderRow(currentContact)
+  
+});
+
+
+
+
 
 // Your code goes here ...
 
 
   
   // ITERATION 2 - Delete Buttons
-  
-  // Your code goes here ...
-  
-  
+  const deleteBtn = newRow.querySelector('.btn-delete')
+
+  deleteBtn.addEventListener('click', () => {
+  newRow.remove()
+  })
+
 
   // ITERATION 3 - Like Buttons
 
-  // Your code goes here ...
+const likeBtn =newRow.querySelector('.btn-like')
 
-  
+likeBtn.addEventListener('click', () =>
+{
+  likeBtn.classList.toggle('selected')
+})
+  renderRow(randomContact)
   
 
 
